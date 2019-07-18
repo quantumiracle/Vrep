e4d711f3988db1ff949b455c99c3a5a0b08d4b45
Zihan Ding <zd2418@graphic06.doc.ic.ac.uk>
2019-07-18 20:41:42 +0100

    718

--- sawyer_wrapper.py
+++ sawyer_wrapper.py
@@ -9,11 +9,11 @@ import vrep_sawyer
 import simulator
 
 class Sawyer:
-    def __init__(self, dt = 100e-3 ):
-        r = vrep_sawyer.VrepSawyer(dt)
+    def __init__(self, dt = 100e-3, headless=False):
+        r = vrep_sawyer.VrepSawyer(dt, headless_mode=headless)
         self.action_space = np.zeros(6)
         self.observation_space = np.zeros(9)
-        self.simulator = simulator.Simulator(r,dt,target_x=0,target_y=0,target_z=0,visualize=False)
+        self.simulator = simulator.Simulator(r,dt,target_x=0,target_y=0,target_z=0,visualize=True)
 
     def reset(self,):
         self.simulator.reset()
